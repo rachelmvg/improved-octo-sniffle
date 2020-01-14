1dcd1d6b24fcf128a94f14e2eeeab0b350b61665
Teun van Gils <edu@teunvg.nl>
2020-01-14 12:09:01 +0100

    Generate a random number.

--- game.py
+++ game.py
@@ -1,3 +1,10 @@
 # This is going to be a game!
 # It will a number guessing game: the computer will pick a number
 # And the human will have to guess what number it is.
+
+# First, we need to import the random package
+import random
+
+MAX_NUMBER = 20
+
+number = random.randint(1, MAX_NUMBER)
