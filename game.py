96180f1bdc6abb378fd0feefb178757f88c4a8ad
Joris Vincent <joris.vincent@gmail.com>
2020-01-14 12:08:51 +0100

    Ask for user input

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
 # This is going to be a game!
 # It will a number guessing game: the computer will pick a number
 # And the human will have to guess what number it is.
+
+userGuess = input('What do you think, the number is?')
+
+print(f'You guessed:{userGuess}')
