8eec35317b4da4d25ddf9991937a4c3f3f3d9e4a
Joris Vincent <joris.vincent@gmail.com>
2020-01-14 12:13:55 +0100

    Added feedback to user

--- game.py
+++ game.py
@@ -2,6 +2,12 @@
 # It will a number guessing game: the computer will pick a number
 # And the human will have to guess what number it is.
 
+computersNumber = 7
+
 userGuess = input('What do you think, the number is?')
 
 print(f'You guessed:{userGuess}')
+if int(userGuess) == computersNumber:
+    print(f'That is correct!')
+else:
+    print('That is wrong!')        
